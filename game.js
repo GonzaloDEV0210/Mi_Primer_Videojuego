c4cf78c9ad2043145cd9b7e3b6a2d0a6fa0e317b
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-09-21 20:14:30 -0500

    Elementos del canvas añadido, practicar

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-console.log(maps);
+const canvas_game = document.querySelector("#game");
+const ctx = canvas_game.getContext("2d");
+
+window.addEventListener('load', startGame);
+window.addEventListener('resize', startGame);
+
+function startGame() {
+    const size = Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8);
+    canvas_game.height = size;
+    canvas_game.width = size;
+
+    let elemetsSize = size / 10;
+
+    ctx.font = elemetsSize + "px arial";
+    ctx.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++) {
+        ctx.fillText(emojis['X'], elemetsSize * i, elemetsSize);
+    }
+};
