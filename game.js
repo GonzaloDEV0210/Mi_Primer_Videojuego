c4442b161c72e0a98f5961437cb085230ba2b8ef
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-09-28 19:11:52 -0500

    Funcionalidad de movimiento con el teclado y los botones hecho

--- game.js
+++ game.js
@@ -14,9 +14,8 @@ function startGame() {
     ctx.font = elemetsSize + "px arial";
     ctx.textAlign = 'end';
 
-    let arrayMaps = maps[1].trim().split("\n");
+    let arrayMaps = maps[2].trim().split("\n");
     let arrayMapsNew = arrayMaps.map(row => row.trim().split(""));
-    console.log(arrayMapsNew[0][2]);
 
     for (let i = 1; i <= 10; i++) {
         for (let j = 1; j <= 10; j++) {
@@ -24,3 +23,42 @@ function startGame() {
         };
     };
 };
+
+const up = document.querySelector("#up");
+const left = document.querySelector("#left");
+const rigth = document.querySelector("#rigth");
+const down = document.querySelector("#down");
+
+function tecladoFunctional(event) {
+    switch (event.key) {
+        case 'ArrowUp':
+            console.log('arriba');
+            break;
+        case 'ArrowDown':
+            console.log('abajo');
+            break;
+        case 'ArrowLeft':
+            console.log('izquierda');
+            break;
+        case 'ArrowRight':
+            console.log('derecha');
+            break;
+        default:
+            alert('Solo se juegan con flechas');
+            break;
+    }
+};
+
+window.addEventListener('keydown', tecladoFunctional);
+up.addEventListener('click', ()=>{
+    console.log('arriba');
+});
+left.addEventListener('click', ()=>{
+    console.log('izquierda');
+});
+rigth.addEventListener('click', ()=>{
+    console.log("derecha");
+});
+down.addEventListener("click", ()=>{
+    console.log('abajo');
+});
